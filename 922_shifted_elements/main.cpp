9a5f281afe9485dbe4168a52291942b2cbaad044
Sergii-Right <sergii.ziborov@gmail.com>
2017-02-08 12:26:32 +0200

    922 shifted elements

---
+++ 922_shifted_elements/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int *a = new int[n];
+    for(int i = 1; i <= n; i++){
+        if(i == n){
+            cin >> a[0];
+        }
+        else {
+            cin >> a[i];
+        }
+    }
+
+    for(int i = 0; i < n; i++){
+        cout << a[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
