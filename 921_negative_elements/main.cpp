0c40cec7f30dc6ed78cb97dc42d552e48147759e
Sergii-Right <sergii.ziborov@gmail.com>
2017-02-08 12:12:30 +0200

    921 negative elements

--- 921_negative_elements/main.cpp
+++ 921_negative_elements/main.cpp
@@ -2,8 +2,22 @@
 
 using namespace std;
 
-int main(int argc, char *argv[])
+int main()
 {
-    cout << "Hello World!" << endl;
+    int n, count = 0;
+    double sum = 0;
+    cin >> n;
+    double *a = new double[n];
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+        if(a[i] < 0){
+            count++;
+            sum += a[i];
+        }
+    }
+
+    cout.precision(2);
+    cout << count << " ";
+    cout << fixed << sum << endl;
     return 0;
 }
