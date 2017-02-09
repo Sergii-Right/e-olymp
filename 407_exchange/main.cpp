6de13fdeee4bb0fde71fd4339b5b514663500280
Sergii-Right <sergii.ziborov@gmail.com>
2017-02-09 18:06:25 +0200

    some change

--- 407_exchange/main.cpp
+++ 407_exchange/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-    char ch[3] = {'G', 'C', 'V'};
+    char ch[]{'G', 'C', 'V'};
     int m;
     cin >> m;
     int *k = new int[m];
@@ -13,16 +13,13 @@ int main()
         cin >> k[i];
     }
     sort(k, k+m);
-
     int max = k[m-1];
-    int j = 0;
-    cout << max << endl;
-    for(int i = 0; i < max; i++){
+    for(int i = 1, j = 0; i <= max; i++){
         swap(ch[1], ch[2]);
         swap(ch[0], ch[1]);
-        if(i == k[j] - 1){
+        if(i == k[j]){
             for(int l = 0; l < 3; l++){
-                cout << ch[l];
+                cout <<  ch[l];
             }
             cout << endl;
             j++;
