96fde4c4a6cb29ea2a63cce9513b48733f8f1a18
Sergii-Right <sergii.ziborov@gmail.com>
2017-02-15 19:01:26 +0200

    352 minimum salary

---
+++ 352_minimum_salary/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+double min(vector<double> v){
+    double min = v[0];
+    for(int i = 1; i < (int)v.size(); i++){
+        if(min > v[i]) min = v[i];
+    }
+    return min;
+}
+
+int main()
+{
+    vector<double> vec;
+    string str, s = "";
+    getline(cin, str);
+    for(auto i : str){
+        if(i != ' '){
+            s += i;
+        }
+        else{
+            vec.push_back(stod(s));
+            s = "";
+        }
+    }
+    vec.push_back(stoi(s));
+
+    cout.precision(2);
+    cout << fixed << min(vec) << endl;
+    return 0;
+}
