c3e210d5c2382e86fbefc81ffa0ddc38865efe2a
Sergii-Right <sergii.ziborov@gmail.com>
2017-02-28 09:45:38 +0200

    443

--- 443_icq/main.cpp
+++ 443_icq/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <map>
+#include <set>
 
 using namespace std;
 
@@ -8,6 +9,8 @@ int main()
     int n, k;
     cin >> n >> k;
     map<int, string> mp;
+    set<string> st;
+
     int icq;
     string name;
     for(int i = 0; i < n; i++){
@@ -16,8 +19,12 @@ int main()
     }
 
     for(auto i : mp){
-        cout << i.second << ' ' << i.first << endl;
-        if(--k == 0) break;
+        st.insert(i.second);
+    }
+
+    for(auto i : st){
+        if(k-- == 0) break;
+        cout << i << endl;
     }
 
     return 0;
