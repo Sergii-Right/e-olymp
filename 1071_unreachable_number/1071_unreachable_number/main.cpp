e867f18da04ea867fba44fc6588164ead4074245
Sergii-Right <sergii.ziborov@gmail.com>
2017-04-01 01:46:08 +0300

    1457

--- 1071_unreachable_number/1071_unreachable_number/main.cpp
+++ 1071_unreachable_number/1071_unreachable_number/main.cpp
@@ -18,6 +18,7 @@ int main(int argc, const char * argv[]) {
     }
     std::sort(arr, arr + n);
     long count = 0;
+
     if(arr[0] == 1){
         count++;
         for(int i = 1; i < n; i++){
---
+++ 1457_sorting_station/1457_sorting_station/main.cpp
@@ -0,0 +1,24 @@
+//
+//  main.cpp
+//  1457_sorting_station
+//
+//  Created by Sergii on 31.03.17.
+//  Copyright © 2017 Sergii. All rights reserved.
+//
+
+#include <iostream>
+int main() {
+    int n, m, max1 = 0, el;
+    std::cin >> n >> m;
+    
+    for(int i = 0; i < n; i++){
+        std::cin >> el;
+        if(max1 < el) max1 = el;
+        else if(max1 + el > m) {
+            std::cout << "No" << std::endl;
+            return 0;
+        }
+    }
+    std::cout << "Yes" << std::endl;
+    return 0;
+}
